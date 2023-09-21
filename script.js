993ceabca1d1fba6b1ff995482bc9eb63a5fbcfa
Rashika <104192441+Rashiie@users.noreply.github.com>
2023-09-21 21:23:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
-function addTask()
-{
-    if(inputBox.value=='')
-    {
-        alert("Please add a task!");
-    }
-    else{
-        let li=document.createElement("li");
-        li.innerHTML=inputBox.value;
-        listContainer.appendChild(li);
-        let span=document.createElement("span");
-        span.innerHTML="\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value="";
-    saveData();
-}
+
 
 inputBox.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
